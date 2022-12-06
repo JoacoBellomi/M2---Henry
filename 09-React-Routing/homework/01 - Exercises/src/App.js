afdea74a720696521076bed567240c283e89137e
JoacoBellomi <Bellomijoaquin@gmai.com>
2022-12-06 08:40:16 -0300

    Finished Homework :)

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -5,8 +5,19 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import Shipping from "./components/Shipping/Shipping.jsx";
 import Promotions from "./components/Promotions/Promotions.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
+import { Route, Routes } from "react-router-dom";
 /* eslint-disable */
 
 export default function App() {
-  return <div></div>;
+  return (
+  <div>
+     <NavBar/>
+     <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/shipping' element={<Shipping></Shipping>}></Route>
+      <Route path="/promotions" element={<Promotions></Promotions>}></Route>
+      <Route path="/cruises/:id" element={<CardDetail></CardDetail>}></Route>
+    </Routes>
+   </div>
+   );
 }
