19d0ef1e984e01997d20b143ded09272fa2d58d7
JoacoBellomi <Bellomijoaquin@gmai.com>
2022-12-07 15:33:08 -0300

    Finished Homework :)

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,22 +3,49 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.querySelector("#valor");
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
+
+  contador = store.getState().contador;
+  console.log(contador);
+  valor.innerHTML = "" + contador; 
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
 }
 
 // Ejecutamos la función 'renderContador':
+renderContador();
 
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
-
+store.subscribe(renderContador);
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+
+
+const botonInc = document.querySelector("#incremento");
+const botonDec = document.querySelector("#decremento");
+const botonImpar = document.querySelector("#incrementoImpar");
+const botonAsync = document.querySelector("#incrementoAsync");
+
+botonInc.addEventListener("click", () => {
+  store.dispatch(incremento());
+})
+
+botonDec.addEventListener("click", () => {
+  store.dispatch(decremento());
+})
+
+botonImpar.addEventListener("click", () => {
+  valor.innerHTML % 2 !== 0 && store.dispatch(incremento())
+})
+
+botonAsync.addEventListener("click", () => {
+  // store.dispatch(incrementoAsync())
+})
